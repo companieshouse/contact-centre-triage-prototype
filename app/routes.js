40c9d69e1c4909148ee1ba35792d153152dc3dd6
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-10-28 16:08:59 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,42 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
+
 // Add your routes here
+
+// start page to first question
+router.post('/v1/start', function (req, res) {
+
+    res.redirect('question-one')
+  
+  })
+
+  // question 1 radio options routing - incomplete
+router.post('/question-one', function(request, response) {
+
+    var questionOne = request.session.data['whatDoYouNeed']
+    if (questionOne == "register"){
+        response.redirect("/v1/register-outcome")
+    }if (questionOne == "filing"){
+            response.redirect("/v1/filing-question")
+    } else {
+        response.redirect("/v1/generic-outcome")
+    }
+})
+
+// filing filter question radio routing - incomplete
+
+router.post('/filing-question', function(request, response) {
+
+    var filingQuestion = request.session.data['filingFilter']
+    if (filingQuestion == "confirmationStatement"){
+        response.redirect("/v1/outcome-conf-stat")
+    }if (filingQuestion == "filingAccounts"){
+            response.redirect("/v1/outcome-accounts")
+    } else {
+        response.redirect("/v1/generic-outcome")
+    }
+})
+
+// test-change
