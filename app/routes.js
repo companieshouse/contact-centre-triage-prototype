ab61c86105ac895917e31ae15ff54c054eed9fbc
Phoebe Radford (Companies House) <pradford@companieshouse.gov.uk>
2024-11-05 11:49:07 +0000

    routing typo fix

--- app/routes.js
+++ app/routes.js
@@ -66,7 +66,7 @@ router.post('/penalties-question', function(request, response) {
     var penaltiesQuestion = request.session.data['penaltiesFilter']
     if (penaltiesQuestion == "appealingPenalty"){
         response.redirect("/v1/outcome-appeal")
-    } else if (penaltiesQuestion == "penaltiesSomthingElse"){
+    } else if (penaltiesQuestion == "penaltiesSomethingElse"){
             response.redirect("/v1/outcome-something-else")
     } else {
         response.redirect("/v1/generic-outcome")
