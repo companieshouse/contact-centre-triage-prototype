7baa59e19ffe4801d4ec405e17a9aedc4af9e674
phoeberadford <pradford@companieshouse.gov.uk>
2025-02-05 19:16:23 +0000

    v4 updates

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,18 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-router.use('/', require('./routes/routes-v1.js'))
-router.use('/', require('./routes/routes-v2.js'))
-router.use('/', require('./routes/routes-v3.js'))
+//router.use('/', require('./routes/routes-v1.js'))
+//router.use('/', require('./routes/routes-v2.js'))
+//router.use('/', require('./routes/routes-v3.js'))
+router.use('/', require('./routes/routes-v4.js'))
+
+// Show session data and URLs in the terminal  
+router.use((req, res, next) => {  
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+    next()  
+  }) 
--- app/routes/routes-v4.js
+++ app/routes/routes-v4.js
@@ -12,113 +12,169 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 
-// version 3 routing
+// version 4 routing
 
 // start page to first question
-router.post('/v3/start', function (req, res) {
+router.post('/v4/start', function (req, res) {
 
-    res.redirect('/v3/question-one')
+    res.redirect('/v4/question-one')
   
   })
 
   // question 1 radio options routing - incomplete
-router.post('/v3/question-one', function(request, response) {
+router.post('/v4/question-one', function(request, response) {
 
     var questionOne = request.session.data['whatDoYouNeed']
     if (questionOne == "register"){
-        response.redirect("/v3/register-outcome")
+        response.redirect("/v4/register-question")
     } else if (questionOne == "filing"){
-            response.redirect("/v3/filing-question")
-    } else if (questionOne == "penalties"){
-        response.redirect("/v3/penalties-question")
+            response.redirect("/v4/filing-question")
+    } else if (questionOne == "maintaining"){
+        response.redirect("/v4/generic-outcome")
+    }
+    else if (questionOne == "penalties"){
+        response.redirect("/v4/penalties-question")
+    }
+    else if (questionOne == "tech"){
+        response.redirect("/v4/generic-outcome")
     }
     else if (questionOne == "report"){
-        response.redirect("/v3/company-complaint-question")
+        response.redirect("/v4/company-complaint-question")
     } else if (questionOne == "somethingElse"){
-        response.redirect("/v3/outcome-something-else")
+        response.redirect("/v4/outcome-something-else")
     }
     else {
-        response.redirect("/v3/generic-outcome")
+        response.redirect("/v4/generic-outcome")
+    }
+})
+
+
+// registering filter question radio routing 
+
+router.post('/v4/register-question', function(request, response) {
+
+    var registerQuestion = request.session.data['registerFilter']
+    if (registerQuestion == "registeringCompany"){
+        response.redirect("/v4/register-outcome")
+    } else if (registerQuestion == "authCode1"){
+            response.redirect("/v4/generic-outcome")
+    }
+     else {
+        response.redirect("/v4/outcome-something-else")
     }
 })
 
 // filing filter question radio routing - incomplete
 
-router.post('/v3/filing-question', function(request, response) {
+router.post('/v4/filing-question', function(request, response) {
 
     var filingQuestion = request.session.data['filingFilter']
     if (filingQuestion == "confirmationStatement"){
-        response.redirect("/v3/outcome-conf-stat")
+        response.redirect("/v4/outcome-conf-stat")
     } else if (filingQuestion == "filingAccounts"){
-            response.redirect("/v3/outcome-accounts")
-    } else if (filingQuestion == "changeFiling"){
-        response.redirect("/v3/filing-sub-question")
-    } else if (filingQuestion == "filingLogIn"){
-        response.redirect("/v3/outcome-sign-in")
+            response.redirect("/v4/outcome-accounts")
     } else if (filingQuestion == "trackFiling"){
-        response.redirect("/v3/outcome-track-filing")
+        response.redirect("/v4/outcome-track-filing")
+    } else if (filingQuestion == "requestingTime"){
+        response.redirect("/v4/generic-outcome")
     } else if (filingQuestion == "somethingElse"){
-        response.redirect("/v3/outcome-something-else")
+        response.redirect("/v4/outcome-something-else")
     } 
      else {
-        response.redirect("/v3/generic-outcome")
+        response.redirect("/v4/generic-outcome")
+    }
+})
+
+
+
+// maintain filter question radio routing 
+
+router.post('/v4/maintain-question', function(request, response) {
+
+    var maintainQuestion = request.session.data['maintainFilter']
+    if (maintainQuestion == "registeredEmailAddress"){
+        response.redirect("/v4/generic-outcome")
+    } else if (maintainQuestion == "registeredOfficeAddress"){
+            response.redirect("/v4/generic-outcome")
+    } else if (maintainQuestion == "directors"){
+        response.redirect("/v4/generic-outcome")
+    } else if (maintainQuestion == "secretaries"){
+        response.redirect("/v4/generic-outcome")
+    } 
+    else if (maintainQuestion == "personsSignificantControl"){
+        response.redirect("/v4/generic-outcome")
+    } 
+    else if (maintainQuestion == "shareCapital"){
+        response.redirect("/v4/outcome-shares")
+    } else if (maintainQuestion == "companyName"){
+        response.redirect("/v4/generic-outcome")
+    } 
+    else if (maintainQuestion == "trackFiling2"){
+        response.redirect("/v4/outcome-track-filing")
+    }  else if (maintainQuestion == "somethingElse"){
+        response.redirect("/v4/outcome-something-else")
+    } 
+     else {
+        response.redirect("/v4/generic-outcome")
     }
 })
 
 
 // filing update or change sub question routing
 
-router.post('/v3/filing-sub-question', function(request, response) {
+// router.post('/v4/filing-sub-question', function(request, response) {
 
-    var updateQuestion = request.session.data['updateFilter']
-    if (updateQuestion == "directorUpdate"){
-        response.redirect("/v3/generic-outcome")
-    } else if (updateQuestion == "pscChanges"){
-            response.redirect("/v3/generic-outcome")
-    } else if (updateQuestion == "fileShares"){
-        response.redirect("/v3/outcome-shares")
-    } else if (updateQuestion == "somethingElse"){
-        response.redirect("/v3/outcome-something-else")
-    } 
-     else {
-        response.redirect("/v3/generic-outcome")
-    }
-})
+//     var updateQuestion = request.session.data['updateFilter']
+//     if (updateQuestion == "directorUpdate"){
+//         response.redirect("/v4/generic-outcome")
+//     } else if (updateQuestion == "pscChanges"){
+//             response.redirect("/v4/generic-outcome")
+//     } else if (updateQuestion == "fileShares"){
+//         response.redirect("/v4/outcome-shares")
+//     } else if (updateQuestion == "somethingElse"){
+//         response.redirect("/v4/outcome-something-else")
+//     } 
+//      else {
+//         response.redirect("/v4/generic-outcome")
+//     }
+// })
+
+
 
 
 
 
 // penalties filter question radio routing - incomplete
 
-router.post('/v3/penalties-question', function(request, response) {
+router.post('/v4/penalties-question', function(request, response) {
 
     var penaltiesQuestion = request.session.data['penaltiesFilter']
     if (penaltiesQuestion == "appealingPenalty"){
-        response.redirect("/v3/outcome-appeal")
+        response.redirect("/v4/outcome-appeal")
     } else if (penaltiesQuestion == "penaltiesSomethingElse"){
-            response.redirect("/v3/outcome-something-else")
+            response.redirect("/v4/outcome-something-else")
     } else {
-        response.redirect("/v3/generic-outcome")
+        response.redirect("/v4/generic-outcome")
     }
 })
 
 
 // report filter question radio routing - incomplete
 
-router.post('/v3/company-complaint-question', function(request, response) {
+router.post('/v4/company-complaint-question', function(request, response) {
 
     var complaintQuestion = request.session.data['complaintFilter']
     if (complaintQuestion == "usingPersonalDetails"){
-        response.redirect("/v3/outcome-personal-details")
+        response.redirect("/v4/outcome-personal-details")
     } else if (complaintQuestion == "complaintSomethingElse"){
-            response.redirect("/v3/outcome-something-else")
+            response.redirect("/v4/outcome-something-else")
     } else if (complaintQuestion == "cantSignIn"){
-        response.redirect("/v3/outcome-sign-in")
+        response.redirect("/v4/outcome-sign-in")
     } else if (complaintQuestion == "companyComplaint"){
-        response.redirect("/v3/outcome-company-complain")
+        response.redirect("/v4/outcome-company-complain")
     }
     else {
-        response.redirect("/v3/generic-outcome")
+        response.redirect("/v4/generic-outcome")
     }
 })
 module.exports=router;
