9f14b0b4a0c86b9b9ca319a81cefb7feaa0c1f02
phoeberadford <pradford@companieshouse.gov.uk>
2025-04-01 17:03:51 +0100

    v6 creation

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,7 @@ router.use('/', require('./routes/routes-v2.js'))
 router.use('/', require('./routes/routes-v3.js'))
 router.use('/', require('./routes/routes-v4.js'))
 router.use('/', require('./routes/routes-v5.js'))
-
+router.use('/', require('./routes/routes-v6.js'))
 
 // Show session data and URLs in the terminal  
 router.use((req, res, next) => {  
